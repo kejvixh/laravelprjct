c3bd1ff3ad11f1e7685376b4daf8fe8e5b5cdd7a
unknown <kejvixhelilaj@outlook.com>
2021-04-01 14:02:16 +0200

    Updated files

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function(){
-    return view('about');
 
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
